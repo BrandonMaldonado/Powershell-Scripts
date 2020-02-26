ce6f138f49de9bd6d678ca53e23aff5694d413e0
Brandon Maldonado <57585700+BrandonMaldonado@users.noreply.github.com>
2020-02-25 23:41:15 -0800

    Update MileStoneCameraMonitor.ps1
    Adjusted HTML styling placement.

--- MileStoneCameraMonitor.ps1
+++ MileStoneCameraMonitor.ps1
@@ -5,44 +5,6 @@ $Cc = "propteam@domain.com"
 $Subject = (Get-Date).ToString("%d-MMMM-yyyy") + " - Weekly Report - Offline Cameras"
 $SMTPServer = "mail.domain.com"
 
-# Import Milestone Config
-$xmlLocation = 'C:\ProgramData\Milestone\Milestone Surveillance\configuration.xml'
-$deviceObjs = Select-Xml -Path $xmlLocation -XPath "//config/Devices/node()" | Select-Object -ExpandProperty node | Where-Object {$_.Name -ne '#whitespace'}
-
-# Declare Report
-$report = @()
-
-# Go through each device, test, and report on offline cameras
-foreach ($device in $deviceObjs){
-    Write-Host "Testing $($device.DisplayName)"
-
-    # Test points - Camera and Firewall
-    $camTestResult = (Test-NetConnection $device.IPAddress -Port 80 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded
-    $gwTestResult = (Test-NetConnection $($device.IPAddress -replace "\.\d{1,3}$",".1") -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).PingSucceeded
-
-    # Check Status
-    if($camTestResult -eq $true){
-        Write-Host -ForegroundColor Green "  Status: Online"
-    }
-
-    if(($camTestResult -eq $false) -and ($gwTestResult -eq $true)){
-        # Debug Status
-        Write-Host -ForegroundColor Red "  Firewall Status: $gwTestResult"
-        Write-Host -ForegroundColor Red "  Camera Status: $camTestResult"
-        
-        # Add to Report
-        $item = New-Object PSObject
-        $item | Add-Member -type NoteProperty -Name 'Camera Name' -Value $($device.DisplayName)
-        $item | Add-Member -type NoteProperty -Name 'Camera IP' -Value $($device.IPAddress)
-        $item | Add-Member -type NoteProperty -Name 'Online' -Value $($camTestResult)
-        $report += $item
-    }
-    elseif(($camTestResult -eq $false) -and ($gwTestResult -eq $false)){
-        Write-Host 'Status: Tunnel Down'
-    }
-}
-
-
 # Define HTML Report Header Style
 $Header = @"
 <style>
@@ -55,8 +17,45 @@ tr:nth-child(odd) {background: #FFF}
 </style>
 "@
 
-if($report){
-  $HTMLReport = $report | Sort-Object 'Camera Name' | ConvertTo-Html -property 'Camera Name','Camera IP','Online' -Head $Header -Pre "<h2>Weekly Report - Offline Cameras</h2><p><b>Generated:</b> $(get-date)</p>"
+# Import Milestone Config
+$xmlLocation = 'C:\ProgramData\Milestone\Milestone Surveillance\configuration.xml'
+$deviceObjs = Select-Xml -Path $xmlLocation -XPath "//config/Devices/node()" | Select-Object -ExpandProperty node | Where-Object { $_.Name -ne '#whitespace' }
 
-  Send-MailMessage -To $To -Cc $Cc -From $From -SmtpServer $SMTPServer -Subject $Subject -Body ($HTMLReport | Out-String) -BodyAsHtml
+# Declare Report
+$report = @()
+
+# Go through each device, test, and report on offline cameras
+foreach ($device in $deviceObjs) {
+    Write-Host "Testing $($device.DisplayName)"
+
+    # Test points - Camera and Firewall
+    $camTestResult = (Test-NetConnection $device.IPAddress -Port 80 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded
+    $gwTestResult = (Test-NetConnection $($device.IPAddress -replace "\.\d{1,3}$", ".1") -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).PingSucceeded
+
+    # Check Status
+    if ($camTestResult -eq $true) {
+        Write-Host -ForegroundColor Green "  Status: Online"
+    }
+
+    if (($camTestResult -eq $false) -and ($gwTestResult -eq $true)) {
+        # Debug Status
+        Write-Host -ForegroundColor Red "  Firewall Status: $gwTestResult"
+        Write-Host -ForegroundColor Red "  Camera Status: $camTestResult"
+        
+        # Add to Report
+        $item = New-Object PSObject
+        $item | Add-Member -type NoteProperty -Name 'Camera Name' -Value $($device.DisplayName)
+        $item | Add-Member -type NoteProperty -Name 'Camera IP' -Value $($device.IPAddress)
+        $item | Add-Member -type NoteProperty -Name 'Online' -Value $($camTestResult)
+        $report += $item
+    }
+    elseif (($camTestResult -eq $false) -and ($gwTestResult -eq $false)) {
+        Write-Host 'Status: Tunnel Down'
+    }
+}
+
+if ($report) {
+    $HTMLReport = $report | Sort-Object 'Camera Name' | ConvertTo-Html -property 'Camera Name', 'Camera IP', 'Online' -Head $Header -Pre "<h2>Property Weekly Report - Offline MDF Cameras</h2><p><b>Generated:</b> $(get-date)</p>"
+
+    Send-MailMessage -To $To -Cc $Cc -From $From -SmtpServer $SMTPServer -Subject $Subject -Body ($HTMLReport | Out-String) -BodyAsHtml
 }
