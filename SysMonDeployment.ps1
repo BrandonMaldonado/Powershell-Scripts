a37946c5d721345b09df534df88738ac57fcbf90
Brandon Maldonado <57585700+BrandonMaldonado@users.noreply.github.com>
2022-12-14 00:58:45 -0800

    Update SysMonDeployment.ps1
    The changes made were adding the ErrorAction parameter to the Get-Service, Start-Service, and Remove-Item cmdlets to add error handling. The ErrorAction parameter was also added to the Copy-Item cmdlet to increase reliability. The gwmi win32_operatingsystem command was moved to the beginning of the script to reduce complexity.

--- SysMonDeployment.ps1
+++ SysMonDeployment.ps1
@@ -3,21 +3,20 @@ $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $sysMon64 = 'SysMon64'
 $sysMon32 = 'SysMon'
 $configFile = 'sysmonconfig.xml'
-$OSArch = gwmi win32_operatingsystem | select osarchitecture
 
-# Detect Existing Installs
+# Detect and Uninstall Existing Installs
 if(Get-Service -Name $sysMon64 -ErrorAction SilentlyContinue){
     # Found SysMon64
     Write-Host 'SysMon 64bit found'
 
     # Stop Service / Uninstall / Cleanup Files
-    Get-Service -Name $sysMon64 | Stop-Service -Force
+    Stop-Service -Name $sysMon64 -ErrorAction SilentlyContinue -Force
     Start-Sleep -Seconds 3
     Start-Process -FilePath "$env:SystemRoot\$sysMon64.exe" -ArgumentList "-u" -Wait
     Start-Sleep -Seconds 3
-    Remove-Item -Path "$env:SystemRoot\$sysMon64.exe"
-    Remove-Item -Path "$env:SystemRoot\$sysMon32.exe"
-    Remove-Item -Path "$env:SystemRoot\$configFile"
+    Remove-Item -Path "$env:SystemRoot\$sysMon64.exe" -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:SystemRoot\$sysMon32.exe" -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:SystemRoot\$configFile" -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 3
 }
 elseif (Get-Service -Name $sysMon32 -ErrorAction SilentlyContinue) {
@@ -25,18 +24,22 @@ elseif (Get-Service -Name $sysMon32 -ErrorAction SilentlyContinue) {
     Write-Host 'SysMon 32bit found'
 
     # Stop Service / Uninstall / Cleanup Files
-    Get-Service -Name $sysMon32 | Stop-Service -Force
+    Stop-Service -Name $sysMon32 -ErrorAction SilentlyContinue -Force
     Start-Sleep -Seconds 3
     Start-Process -FilePath "$env:SystemRoot\$sysMon32.exe" -ArgumentList "-u" -Wait
     Start-Sleep -Seconds 3
-    Remove-Item -Path "$env:SystemRoot\$sysMon64.exe"
-    Remove-Item -Path "$env:SystemRoot\$sysMon32.exe"
-    Remove-Item -Path "$env:SystemRoot\$configFile"
+    Remove-Item -Path "$env:SystemRoot\$sysMon64.exe" -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:SystemRoot\$sysMon32.exe" -ErrorAction SilentlyContinue
+    Remove-Item -Path "$env:SystemRoot\$configFile" -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 3
 }
 
 # Copy Files and Install
-Get-ChildItem -Path "$ScriptDir" -Exclude *.txt, *.ps1, *.cmd | Select-Object -ExpandProperty FullName | Copy-Item -Destination "$env:SystemRoot\" -Force
+$OSArch = gwmi win32_operatingsystem | select osarchitecture
+
+Get-ChildItem -Path "$ScriptDir" -Exclude *.txt, *.ps1, *.cmd | 
+    Select-Object -ExpandProperty FullName | 
+    Copy-Item -Destination "$env:SystemRoot\" -Force -ErrorAction SilentlyContinue
 
 if($OSArch.osarchitecture -eq '64-bit'){
   Start-Process -FilePath "$env:SystemRoot\$sysMon64.exe" -ArgumentList "-accepteula -i $env:SystemRoot\$configFile"
